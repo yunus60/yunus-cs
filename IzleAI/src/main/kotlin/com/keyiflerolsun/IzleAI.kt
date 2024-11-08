94dad7f3f44e97569630266ff80f97dcc89409b7
keyiflerolsun <keyiflerolsun@gmail.com>
2024-11-08 12:29:27 +0300

    🕊
    🕊

--- IzleAI/src/main/kotlin/com/keyiflerolsun/IzleAI.kt
+++ IzleAI/src/main/kotlin/com/keyiflerolsun/IzleAI.kt
@@ -19,17 +19,31 @@ class IzleAI : MainAPI() {
     override val hasDownloadSupport   = true
     override val supportedTypes       = setOf(TvType.Movie)
 
+    // ! CloudFlare bypass
+    override var sequentialMainPage = true        // * https://recloudstream.github.io/dokka/-cloudstream/com.lagradost.cloudstream3/-main-a-p-i/index.html#-2049735995%2FProperties%2F101969414
+    override var sequentialMainPageDelay       = 50L  // ? 0.05 saniye
+    override var sequentialMainPageScrollDelay = 50L  // ? 0.05 saniye
+
     override val mainPage = mainPageOf(
         "${mainUrl}/kategori/aile-filmleri"        to "Aile",
         "${mainUrl}/kategori/aksiyon-filmleri"     to "Aksiyon",
         "${mainUrl}/kategori/animasyon-filmleri"   to "Animasyon",
         "${mainUrl}/kategori/belgesel-filmleri"    to "Belgesel",
         "${mainUrl}/kategori/bilim-kurgu-filmleri" to "Bilim Kurgu",
+        "${mainUrl}/kategori/dram-filmleri"        to "Dram",
         "${mainUrl}/kategori/fantastik-filmleri"   to "Fantastik",
+        "${mainUrl}/kategori/film-noir-filmleri"   to "Film-Noir",
+        "${mainUrl}/kategori/gerilim-filmleri"     to "Gerilim",
+        "${mainUrl}/kategori/gizem-filmleri"       to "Gizem",
         "${mainUrl}/kategori/kisa-filmleri"        to "Kısa Film",
         "${mainUrl}/kategori/komedi-filmleri"      to "Komedi",
+        "${mainUrl}/kategori/korku-filmleri"       to "Korku",
         "${mainUrl}/kategori/macera-filmleri"      to "Macera",
-        "${mainUrl}/kategori/romantik-filmleri"    to "Romantik"
+        "${mainUrl}/kategori/muzik-filmleri"       to "Müzik",
+        "${mainUrl}/kategori/romantik-filmleri"    to "Romantik",
+        "${mainUrl}/kategori/savas-filmleri"       to "Savaş",
+        "${mainUrl}/kategori/spor-filmleri"        to "Spor",
+        "${mainUrl}/kategori/suc-filmleri"         to "Suç",
     )
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
