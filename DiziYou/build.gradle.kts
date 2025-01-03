a90c024ed21f541818523b7d3425de8d2a14e8af
keyiflerolsun <keyiflerolsun@gmail.com>
2025-01-04 02:11:35 +0300

    🕊
    🕊

---
+++ DiziYou/build.gradle.kts
@@ -0,0 +1,18 @@
+version = 1
+
+cloudstream {
+    authors     = listOf("keyiflerolsun")
+    language    = "tr"
+    description = "Diziyou en kaliteli Türkçe dublaj ve altyazılı yabancı dizi izleme sitesidir. Güncel ve efsanevi dizileri 1080p Full HD kalitede izlemek için hemen tıkla!"
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+    **/
+    status  = 1 // will be 3 if unspecified
+    tvTypes = listOf("TvSeries")
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.diziyou.co&sz=%size%"
+}
---
+++ DiziYou/src/main/kotlin/com/keyiflerolsun/DiziYou.kt
@@ -0,0 +1,155 @@
+// ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
+
+package com.keyiflerolsun
+
+import android.util.Log
+import org.jsoup.nodes.Element
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
+import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
+
+class DiziYou : MainAPI() {
+    override var mainUrl              = "https://www.diziyou.co"
+    override var name                 = "DiziYou"
+    override val hasMainPage          = true
+    override var lang                 = "tr"
+    override val hasQuickSearch       = false
+    override val hasChromecastSupport = true
+    override val hasDownloadSupport   = true
+    override val supportedTypes       = setOf(TvType.TvSeries)
+
+    override val mainPage = mainPageOf(
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Aile"                 to "Aile",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Aksiyon"              to "Aksiyon",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Animasyon"            to "Animasyon",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Belgesel"             to "Belgesel",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Bilim+Kurgu"          to "Bilim Kurgu",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Dram"                 to "Dram",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Fantazi"              to "Fantazi",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Gerilim"              to "Gerilim",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Gizem"                to "Gizem",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Komedi"               to "Komedi",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Korku"                to "Korku",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Macera"               to "Macera",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Sava%C5%9F"           to "Savaş",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Su%C3%A7"             to "Suç",
+        "${mainUrl}/dizi-arsivi/page/SAYFA/?tur=Vah%C5%9Fi+Bat%C4%B1" to "Vahşi Batı"
+    )
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
+        val url      = request.data.replace("SAYFA", "${page}")
+        val document = app.get(url).document
+        val home     = document.select("div.single-item").mapNotNull { it.toMainPageResult() }
+
+        return newHomePageResponse(request.name, home)
+    }
+
+    private fun Element.toMainPageResult(): SearchResponse? {
+        val title     = this.selectFirst("div#categorytitle a")?.text() ?: return null
+        val href      = fixUrlNull(this.selectFirst("div#categorytitle a")?.attr("href")) ?: return null
+        val posterUrl = fixUrlNull(this.selectFirst("img")?.attr("src"))
+
+        return newTvSeriesSearchResponse(title, href, TvType.TvSeries) { this.posterUrl = posterUrl }
+    }
+
+    override suspend fun search(query: String): List<SearchResponse> {
+        val document = app.get("${mainUrl}/?s=${query}").document
+
+        return document.select("div.incontent div#list-series").mapNotNull { it.toMainPageResult() }
+    }
+
+    override suspend fun quickSearch(query: String): List<SearchResponse> = search(query)
+
+    override suspend fun load(url: String): LoadResponse? {
+        val document = app.get(url).document
+
+        val title           = document.selectFirst("h1")?.text()?.trim() ?: return null
+        val poster          = fixUrlNull(document.selectFirst("div.category_image img")?.attr("src"))
+        val description     = document.selectFirst("div.diziyou_desc")?.ownText()?.trim()
+        val year            = document.selectFirst("span.dizimeta:contains(Yapım Yılı)")?.nextSibling()?.toString()?.trim()?.toIntOrNull()
+        val tags            = document.select("div.genres a").map { it.text() }
+        val rating          = document.selectFirst("span.dizimeta:contains(IMDB)")?.nextSibling()?.toString()?.trim()?.toRatingInt()
+        val actors          = document.selectFirst("span.dizimeta:contains(Oyuncular)")?.nextSibling()?.toString()?.trim()?.split(", ")?.map { Actor(it) }
+        val trailer         = document.selectFirst("iframe.trailer-video")?.attr("src")
+
+        val episodes = document.select("div.bolumust").mapNotNull {
+            val epName    = it.selectFirst("div.baslik")?.ownText()?.trim() ?: return@mapNotNull null
+            val epHref    = it.closest("a")?.attr("href")?.let { href -> fixUrlNull(href) } ?: return@mapNotNull null
+            val epEpisode = Regex("""(\d+)\. Bölüm""").find(epName)?.groupValues?.get(1)?.toIntOrNull()
+            val epSeason  = Regex("""(\d+)\. Sezon""").find(epName)?.groupValues?.get(1)?.toIntOrNull() ?: 1
+
+            Episode(
+                data    = epHref,
+                name    = epName + " » " + it.selectFirst("div.bolumismi")?.text()?.trim(),
+                season  = epSeason,
+                episode = epEpisode
+            )
+        }
+
+        return newTvSeriesLoadResponse(title, url, TvType.TvSeries, episodes) {
+            this.posterUrl = poster
+            this.plot      = description
+            this.year      = year
+            this.tags      = tags
+            this.rating    = rating
+            addActors(actors)
+            addTrailer(trailer)
+        }
+    }
+
+    override suspend fun loadLinks(data: String, isCasting: Boolean, subtitleCallback: (SubtitleFile) -> Unit, callback: (ExtractorLink) -> Unit): Boolean {
+        Log.d("DZY", "data » ${data}")
+        val document = app.get(data).document
+
+        val itemId     = document.selectFirst("iframe#diziyouPlayer")?.attr("src")?.split("/")?.lastOrNull()?.substringBefore(".html") ?: return false
+        Log.d("DZY", "itemId » ${itemId}")
+
+        val subTitles  = mutableListOf<DiziyouSubtitle>()
+        val streamUrls = mutableListOf<DiziyouStream>()
+
+        document.select("span.diziyouOption").forEach {
+            val optId   = it.attr("id")
+            val optName = it.text()
+
+            if (optId == "turkceAltyazili") {
+                subTitles.add(DiziyouSubtitle("Turkish", "https://storage.diziyou.co/subtitles/${itemId}/tr.vtt"))
+                streamUrls.add(DiziyouStream("Orjinal Dil", "https://storage.diziyou.co/episodes/${itemId}/play.m3u8"))
+            }
+            if (optId == "ingilizceAltyazili") {
+                subTitles.add(DiziyouSubtitle("English", "https://storage.diziyou.co/subtitles/${itemId}/en.vtt"))
+                streamUrls.add(DiziyouStream("Orjinal Dil", "https://storage.diziyou.co/episodes/${itemId}/play.m3u8"))
+            }
+            if (optId == "turkceDublaj") {
+                streamUrls.add(DiziyouStream("Türkçe Dublaj", "https://storage.diziyou.co/episodes/${itemId}_tr/play.m3u8"))
+            }
+        }
+
+        for (sub in subTitles) {
+            subtitleCallback.invoke(
+                SubtitleFile(
+                    lang = sub.name,
+                    url  = fixUrl(sub.url)
+                )
+            )
+        }
+
+        for (stream in streamUrls) {
+            callback.invoke(
+                ExtractorLink(
+                    source  = stream.name,
+                    name    = stream.name,
+                    url     = stream.url,
+                    referer = "${mainUrl}/",
+                    quality = Qualities.Unknown.value,
+                    isM3u8  = true
+                )
+            )
+        }
+
+        return true
+    }
+
+    data class DiziyouSubtitle(val name: String, val url: String)
+    data class DiziyouStream(val name: String, val url: String)
+}
