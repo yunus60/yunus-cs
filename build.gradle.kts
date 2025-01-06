2f720853526c3b0be347969ec3b9fa438088e7ed
inatchii <129805654+inatchii@users.noreply.github.com>
2025-01-07 00:02:51 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/keyiflerolsun/Kekik-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/inatchii/Kekik-cloudstream")
 
         authors = listOf("keyiflerolsun")
     }
