ec6b56c72a2f5c5d0b91a2c4fa26e77c27eb9300
keyiflerolsun <keyiflerolsun@gmail.com>
2025-02-01 08:09:53 +0300

    🕊
    🕊

--- BelgeselX/src/main/kotlin/com/keyiflerolsun/BelgeselX.kt
+++ BelgeselX/src/main/kotlin/com/keyiflerolsun/BelgeselX.kt
@@ -65,15 +65,15 @@ class BelgeselX : MainAPI() {
     override suspend fun search(query: String): List<SearchResponse> {
         val cx = "016376594590146270301:iwmy65ijgrm" //Might change in the future
         val tokenResponse = app.get("https://cse.google.com/cse.js?cx=${cx}")
-        val cseLibVersion = Regex("""cselibVersion": "(.*)"""").find(tokenResponse.text)?.groupValues?.get(1)
-        val cseToken = Regex("""cse_token": "(.*)"""").find(tokenResponse.text)?.groupValues?.get(1)
+        val cseLibVersion = Regex("""cselibVersion\": \"(.*)\"""").find(tokenResponse.text)?.groupValues?.get(1)
+        val cseToken = Regex("""cse_token\": \"(.*)\"""").find(tokenResponse.text)?.groupValues?.get(1)
 
         val response = app.get("https://cse.google.com/cse/element/v1?rsz=filtered_cse&num=100&hl=tr&source=gcsc&cselibv=${cseLibVersion}&cx=${cx}&q=${query}&safe=off&cse_tok=${cseToken}&oq=${query}&callback=google.search.cse.api9969&rurl=https%3A%2F%2Fbelgeselx.com%2F")
         Log.d("BLX","Search result: ${response.text}")
 
-        val titles = Regex(""""titleNoFormatting": "(.*)"""").findAll(response.text).map { it.groupValues[1] }.toList()
-        val urls = Regex(""""url": "(.*)"""").findAll(response.text).map { it.groupValues[1] }.toList()
-        val posterUrls = Regex(""""ogImage": "(.*)"""").findAll(response.text).map { it.groupValues[1] }.toList()
+        val titles = Regex("""\"titleNoFormatting\": \"(.*)\"""").findAll(response.text).map { it.groupValues[1] }.toList()
+        val urls = Regex("""\"url\": \"(.*)\"""").findAll(response.text).map { it.groupValues[1] }.toList()
+        val posterUrls = Regex("""\"ogImage\": \"(.*)\"""").findAll(response.text).map { it.groupValues[1] }.toList()
 
         val searchResponses = mutableListOf<TvSeriesSearchResponse>()
 
