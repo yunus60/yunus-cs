53b5dce040b96f3e91800e1dd6dbb5d91fa397da
keyiflerolsun <keyiflerolsun@gmail.com>
2025-02-15 19:51:30 +0300

    🕊
    🕊

--- Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
+++ Dizilla/src/main/kotlin/com/keyiflerolsun/Dizilla.kt
@@ -36,7 +36,7 @@ class Dizilla : MainAPI() {
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
         val document = app.get("${request.data}").document
         val home     = if (request.data.contains("dizi-turu")) { 
-            document.select("div.new-added-list a").mapNotNull { it.diziler() }
+            document.select("div.grid-cols-3 a").mapNotNull { it.diziler() }
         } else {
             document.select("div.grid a").mapNotNull { it.sonBolumler() }
         }
