69deda8a5650dc0761080a2c1ed3eb1b350ee16f
keyiflerolsun <keyiflerolsun@users.noreply.github.com>
2025-02-27 17:11:27 +0000

    ♻️ Domain Değişikliği
    🔄 Otomatik domain güncellemeleri yapıldı.

--- DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/keyiflerolsun/DiziPal.kt
@@ -10,7 +10,7 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 
 class DiziPal : MainAPI() {
-    override var mainUrl              = "https://dizipal874.com"
+    override var mainUrl              = "https://dizipal875.com"
     override var name                 = "DiziPal"
     override val hasMainPage          = true
     override var lang                 = "tr"
--- DiziYou/src/main/kotlin/com/keyiflerolsun/DiziYou.kt
+++ DiziYou/src/main/kotlin/com/keyiflerolsun/DiziYou.kt
@@ -10,7 +10,7 @@ import com.lagradost.cloudstream3.LoadResponse.Companion.addActors
 import com.lagradost.cloudstream3.LoadResponse.Companion.addTrailer
 
 class DiziYou : MainAPI() {
-    override var mainUrl              = "https://www.diziyou1.com"
+    override var mainUrl              = "https://www.diziyou2.com"
     override var name                 = "DiziYou"
     override val hasMainPage          = true
     override var lang                 = "tr"
